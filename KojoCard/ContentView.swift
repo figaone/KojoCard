027946754de87a50db256d9546d8f66ae823b1fe
kojo <kgyamfi@iastate.edu>
2022-05-13 14:58:35 -0500

    Initial Commit

---
+++ KojoCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KojoCard
+//
+//  Created by Sharma, Anuj [CCE E] on 5/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KojoCard/KojoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KojoCardApp.swift
+//  KojoCard
+//
+//  Created by Sharma, Anuj [CCE E] on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct KojoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
